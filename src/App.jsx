0b028d77e2031cddecb1df35a05cff8e7958763f
Nebyou <119705382+NebyouMeseret@users.noreply.github.com>
2025-02-17 23:57:57 -0800

    header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Hello!!</h1>
-    </>
-  )
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App
+
+
+// style={{ backgroundColor: "black", color: "white" }}
